93b5b102bb38f8b458d6798ebab414757801c9d7
tantran2802 <88880933+tantran2802@users.noreply.github.com>
2022-11-12 14:38:51 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,26 @@
+import { Route, Routes} from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+//import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path='/' element={<AllMeetupsPage />}>
+          
+        </Route>
+        <Route path='/new-meetup' element={<NewMeetupPage />}>
+          
+        </Route>
+        <Route path='/favorites' element={<FavoritesPage />}>
+          
+        </Route>
+      </Routes>
+    
+      </Layout>
+  );
+}
+
+export default App;
